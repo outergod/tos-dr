846950a41df128895ef9a437f158e6c048abfe0d
Alexander Kahl <e-user@fsfe.org>
2012-09-20 01:06:34 +0100

    added domain injector test client

---
+++ DomainInjector/Program.fs
@@ -0,0 +1,16 @@
+﻿module App
+
+open System.ServiceModel
+open TOSCheckerService.Contracts
+
+[<EntryPoint>]
+let main argv =
+    let factory = new ChannelFactory<ITOSCheckerService>(new BasicHttpBinding(), "http://localhost:8123")
+    let channel = factory.CreateChannel()
+
+    match argv.Length with
+    | 1 ->
+        let domain = argv.[0]
+        channel.LoadDomain domain
+        0
+    | _ -> 1
